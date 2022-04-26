ccd71347aaafeaee15e4f69c9d36202c242dfcb3
Chirag Mangukiya <cdmi.chirag@gmail.com>
2022-04-26 16:39:04 +0530

    sub-category UI

--- src/app/sub-category/sub-category.component.ts
+++ src/app/sub-category/sub-category.component.ts
@@ -14,50 +14,16 @@ export class SubCategoryComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
-    autoWidth: true,
-    nav: true,
-    autoplay: true,
-    // autoplayTimeout: 3000,
-    autoplayHoverPause:true,
-    stagePadding:50,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-        touchDrag: true
-      },
-      425: {
-        items: 1,
-        touchDrag: true
-      },
-      740: {
-        items: 1,
-        touchDrag: true
-      },
-      940: {
-        items: 1
-      }
-    }
-    
-  }
 
-  categorySliderOption: OwlOptions = {
+  subCategorySlider: OwlOptions = {
     loop: true,
+    margin:20,
     mouseDrag: false,
     touchDrag: false,
     pullDrag: false,
     dots: false,
     navSpeed: 700,
     navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
-    autoWidth: true,
     autoplay: true,
     autoplayTimeout: 2000,
     autoplayHoverPause:true,
@@ -83,11 +49,85 @@ export class SubCategoryComponent implements OnInit {
         touchDrag: true
       },
       1000: {
-        items: 10
+        items: 8
       }
 
     },
     nav: true
+  }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
+    autoWidth: true,
+    nav: true,
+    autoplay: true,
+    // autoplayTimeout: 3000,
+    autoplayHoverPause:true,
+    stagePadding:50,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        touchDrag: true
+      },
+      425: {
+        items: 1,
+        nav: false,
+        touchDrag: true
+      },
+      740: {
+        items: 1,
+        nav: false,
+        touchDrag: true
+      },
+      940: {
+        items: 1
+      }
+    }
+    
+  }
+
+  top_brands_banner: OwlOptions = {
+    loop: true,
+    margin:15,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    nav: true,
+    navSpeed: 1000,
+    navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
+    autoWidth: true,
+    autoplay: true,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1,
+        nav:false,
+        touchDrag: true
+      },
+      576:{
+        items: 2,
+        nav:false,
+        touchDrag: true
+      },
+      768: {
+        items: 2,
+        nav:false,
+        touchDrag: true
+      },
+      1000: {
+        items: 2
+      }
+
+    }
+    
   }
 
   top_brands: OwlOptions = {
@@ -127,67 +167,46 @@ export class SubCategoryComponent implements OnInit {
     
   }
 
-  top_brands_banner: OwlOptions = {
+  subCategorySmall: OwlOptions = {
     loop: true,
-    margin:15,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    nav: true,
-    navSpeed: 1000,
-    navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
-    autoWidth: true,
-    autoplay: true,
-    autoplayHoverPause:true,
-    responsive: {
-      0: {
-        items: 1,
-        nav:false,
-        touchDrag: true
-      },
-      576:{
-        items: 2,
-        nav:false,
-        touchDrag: true
-      },
-      768: {
-        items: 2,
-        nav:false,
-        touchDrag: true
-      },
-      1000: {
-        items: 3
-      }
-
-    }
-    
-  }
-
-  flashProducts: OwlOptions = {
-    loop: true,
+    margin:20,
     mouseDrag: false,
     touchDrag: false,
     pullDrag: false,
     dots: false,
     navSpeed: 700,
     navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
-    autoWidth: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:true,
     responsive: {
       0: {
-        items: 1
+        items: 2,
+        nav:false,
+        touchDrag: true
       },
-      400: {
-        items: 1
+      375: {
+        items: 3,
+        nav:false,
+        touchDrag: true
       },
-      740: {
-        items: 1
+      576: {
+        items: 6,
+        nav:false,
+        touchDrag: true
       },
-      940: {
+      768: {
+        items: 7,
+        nav:false,
+        touchDrag: true
+      },
+      1000: {
         items: 6
       }
+
     },
     nav: true
   }
+
 
 }
