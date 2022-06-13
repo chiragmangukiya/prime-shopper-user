6f3caeac78a89fcb1b37c91d3a085bb7c61ac3f6
Chirag Mangukiya <cdmi.chirag@gmail.com>
2022-06-13 18:18:26 +0530

    set image size

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit {
     navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
     autoWidth: true,
     nav: true,
-    autoplay: true,
+    // autoplay: true,
     // autoplayTimeout: 3000,
     autoplayHoverPause:true,
     stagePadding:50,
@@ -71,7 +71,7 @@ export class HomeComponent implements OnInit {
         touchDrag: true
       },
       576: {
-        items: 6,
+        items: 7,
         nav:false,
         touchDrag: true
       },
@@ -82,10 +82,54 @@ export class HomeComponent implements OnInit {
       },
       1000: {
         items: 9
+      },
+      1400: {
+        items: 11
       }
 
     },
     nav: true
+  }
+
+  best_offer: OwlOptions = {
+    loop: true,
+    margin:15,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    nav: true,
+    navSpeed: 1000,
+    navText: [ '<img src="/assets/images/left-arrow.png">', '<img src="/assets/images/right-arrow.png">' ],
+    autoWidth: true,
+    autoplay: true,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 2,
+        center:true,
+        nav:false,
+        touchDrag: true
+      },
+      576: {
+        items: 3,
+        nav:false,
+        touchDrag: true
+      },
+      768: {
+        items: 4,
+        nav:false,
+        touchDrag: true
+      },
+      1000: {
+        items: 5
+      },
+      1366: {
+        items: 6
+      }
+
+    }
+
   }
 
   top_brands: OwlOptions = {
@@ -119,6 +163,9 @@ export class HomeComponent implements OnInit {
       },
       1000: {
         items: 5
+      },
+      1366: {
+        items: 6
       }
 
     }
@@ -187,6 +234,8 @@ export class HomeComponent implements OnInit {
     },
     nav: true
   }
+
+  
 
   allCategories: any = [];
 
